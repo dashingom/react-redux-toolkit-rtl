093333c6d368e9e56eca16e1615a0140f8fc4b4c
Onkar Nawghare <onkar.nawghare@gmail.com>
2023-04-11 11:38:23 +0530

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  parser: "flow",
+  arrowParens: "avoid",
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <p>Test App</p>
+    </>
+  );
+}
+
+export default App;
